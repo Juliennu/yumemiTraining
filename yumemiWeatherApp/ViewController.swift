b06afca81c56033f5f83a3f11af5cdcde228fbac
Juliennu <wsb.1099@gmail.com>
2021-08-10 12:03:14 +0900

    first commit

---
+++ yumemiWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yumemiWeatherApp
+//
+//  Created by Juri Ohto on 2021/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+
+}
+
